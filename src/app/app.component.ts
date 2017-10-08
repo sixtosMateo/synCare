c6cc5a8708006782a25e5ee1bbe39fcba30bf67f
Mateo Sixtos <msixtos@csumb.edu>
2017-10-08 00:34:01 -0700

    added style to dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,7 @@ import { DashboardPage } from '../pages/dashboard/dashboard';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = LoginPage;
 
   pages: Array<{title: string, component: any}>;
 
