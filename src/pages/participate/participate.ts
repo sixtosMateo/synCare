1666ff2fd00673f18a92003fc69c33cb6550a864
garcialuis <luisgarcia@csumb.edu>
2017-10-08 00:32:23 -0700

    Removed toggle menu from participate screen

--- src/pages/participate/participate.ts
+++ src/pages/participate/participate.ts
@@ -188,4 +188,35 @@ export class ParticipatePage {
     });
   }
 
+  denyRequest(){
+
+    console.log("Denied request");
+
+  }
+
+  acceptRequest(){
+
+    var headers = new Headers();
+   headers.append("Accept", 'application/json');
+   headers.append('Content-Type', 'application/json' );
+   let options = new RequestOptions({ headers: headers });
+
+   let dataObj = {
+     title: 'foo',
+     body: 'bar',
+     userId: 1
+   }
+
+   this.http.post("http://jsonplaceholder.typicode.com/posts", dataObj, options)
+     .subscribe(data => {
+       console.log(data['_body']);
+      }, error => {
+       console.log(error);// Error getting the data
+     });
+
+
+    console.log("Accepted request");
+
+  }
+
 }
